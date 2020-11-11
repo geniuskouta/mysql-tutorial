803a0feb2cb03d94859cb6ec9db5d024af6070bd
geniuskouta <knakano95@gmail.com>
2020-11-11 22:50:56 +0900

    query by select

---
+++ src/select_query/dbconfig.php
@@ -0,0 +1,5 @@
+<?php
+    $host = '192.168.99.100';
+    $dbname = 'sample';
+    $username = 'root';
+    $password = 'root';
---
+++ src/select_query/index.php
@@ -0,0 +1,45 @@
+<?php
+    require_once 'dbconfig.php';
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    /* select query */
+    $sql = 'SELECT subject, start_date, description
+    FROM tasks
+    ORDER BY start_date';
+    $q = $pdo->query($sql);
+    $q->setFetchMode(PDO::FETCH_ASSOC);
+} catch (PDOException $pe) {
+    die("Could not connect to the database $dbname :" . $pe->getMessage());
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP MySQL Query Data Demo</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="container">
+            <h1>Employees</h1>
+            <table class="table table-bordered table-condensed">
+                <thead>
+                    <tr>
+                        <th>start_date</th>
+                        <th>subject</th>
+                        <th>desc</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = $q->fetch()): ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($row['start_date']); ?></td>
+                            <td><?php echo htmlspecialchars($row['subject']) ?></td>
+                            <td><?php echo htmlspecialchars($row['description']) ?></td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+    </body>
+</div>
+</html>
